fa17113cd7858d10f16c10daf6080cdc41a6f34e
Lucas Parreira <153011314+lucasnparreira@users.noreply.github.com>
2024-07-13 08:25:16 -0300

    fix: Adjusting routes to render templates

--- app.py
+++ app.py
@@ -1,37 +1,49 @@
-from flask import Flask, request, jsonify
+from flask import Flask, redirect, render_template, request, jsonify, url_for
 import sqlite3
 from models import init_db
 
 app =Flask(__name__)
 init_db()
 
-@app.route('/transactions', methods=['POST'])
-def add_transaction():
-    data = request.get_json()
-    description = data['description']
-    amount = data['amount']
-    type_val = data['type_val']
-    date = data['date']
-
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        INSERT INTO transactions (description, amount, type_val, date)
-        VALUES (?, ?, ?, ?)
-''', (description, amount, type_val, date))
-    conn.commit()
-    conn.close()
-    return jsonify({'message':'Transaction added successfully!'}), 201
-
-@app.route('/transactions', methods=['GET'])
-def get_transactions():
+@app.route('/', methods=['GET'])
+def home():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM transactions')
     transactions = cursor.fetchall()
     conn.close()
 
-    return jsonify(transactions)
+    return render_template('view_transactions.html', transactions=transactions)
+
+
+@app.route('/add', methods=['GET','POST'])
+def add_transaction():
+    if request.method == 'POST':
+        description = request.form['description']
+        amount = request.form['amount']
+        type_val = request.form['type_val']
+        date = request.form['date']
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO transactions (description, amount, type_val, date)
+            VALUES (?, ?, ?, ?)
+    ''', (description, amount, type_val, date))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('view_transactions'))
+    return render_template('add_transaction.html')
+
+@app.route('/transactions', methods=['GET'])
+def view_transactions():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM transactions')
+    transactions = cursor.fetchall()
+    conn.close()
+
+    return render_template('view_transactions.html', transactions=transactions)
 
 @app.route('/transactions/<int:id>', methods=['GET'])
 def get_transaction(id):
@@ -42,29 +54,37 @@ def get_transaction(id):
     conn.close()
 
     if transaction:
-        return jsonify(transaction)
+        return render_template('view_transactions.html')
     else:
         return jsonify({'message': 'Transaction not found!'}), 404
     
-@app.route('/transactions/<int:id>', methods=['PUT'])
-def update_transaction(id):
-    data = request.get_json()
-    description = data['description']
-    amount = data['amount']
-    type_val = data['type_val']
-    date = data['date']
-
+@app.route('/edit/<int:id>', methods=['GET','POST'])
+def edit_transaction(id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('''
-        UPDATE transactions
-        SET description = ?, amount = ?, type_val = ?, date = ?
-        WHERE id = ?
-''', (description, amount, type_val, date, id))
-    conn.commit()
+    if request.method == 'POST':
+        description = request.form['description']
+        amount = request.form['amount']
+        type_val = request.form['type_val']
+        date = request.form['date']
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+            UPDATE transactions
+            SET description = ?, amount = ?, type_val = ?, date = ?
+            WHERE id = ?
+    ''', (description, amount, type_val, date, id))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('view_transactions'))
+    
+    cursor.execute('SELECT * FROM transactions WHERE id = ?', (id,))
+    transaction = cursor.fetchone()
     conn.close()
 
-    return jsonify({'message': 'Transaction updated successfully!'})
+    return render_template('edit_transaction.html', transaction=transaction)
 
 @app.route('/transactions/<int:id>', methods=['DELETE'])
 def delete_transaction(id):
@@ -74,7 +94,7 @@ def delete_transaction(id):
     conn.commit()
     conn.close()
 
-    return jsonify({'message': 'Transaction deleted successfully!'})
+    return redirect(url_for('view_transactions'))
 
 
 if __name__ == '__main__':
